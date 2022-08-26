103d787c7f153d18bd97ad5f0d71e8adb0c10f2c
Caio <caiusito@gmail.com>
2022-08-26 16:36:45 -0300

    iniciando o projeto TrybeWallet

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,11 @@
-// configure aqui sua store
+import { legacy_createStore as createStore } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer, composeWithDevTools);
+
+if (window.Cypress) {
+  window.store = store;
+}
+
+export default store;
