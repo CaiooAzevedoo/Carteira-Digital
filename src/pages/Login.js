26f725d18ee8417742b35944e545e9d1173bf5dd
Caio <caiusito@gmail.com>
2022-08-31 17:57:16 -0300

    retornando ao que funcionou

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -68,7 +68,7 @@ class Login extends React.Component {
         <button
           type="button"
           onClick={ this.loginIn }
-          disabled={ disable }
+          disabled={ disabled }
         >
           Entrar
         </button>
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Header from '../components/Header';
 
 class Wallet extends React.Component {
@@ -31,4 +32,8 @@ Wallet.propTypes = {
   email: PropTypes.string.isRequired,
 };
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Wallet);
